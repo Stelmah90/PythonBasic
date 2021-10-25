91cb2dc1b078f6732398f38262dc2bf143c578df
Alex <stelmah1990@gmail.com>
2021-10-25 18:04:48 +0300

    added Task2

--- Task_2.py
+++ Task_2.py
@@ -1,12 +1,5 @@
-while True:
-    time_sec = int(input('Ввведите время в секундах: '))
-    if time_sec < 0:
-        print('Нельзя отрицательное число')
-    else:
-        hours = str(time_sec // 3600)
-        minutes = str((time_sec - int(hours) * 3600) // 60)
-        seconds = str(time_sec - (int(hours) * 3600) - (int(minutes) * 60))
-        print("{}:{}:{}".format(hours if len(hours) > 1 else '0' + hours,
-                                minutes if len(minutes) > 1 else '0' + minutes,
-                                seconds if len(seconds) > 1 else '0' + seconds))
-        break
+user_list = input('Введите список слов через пробел: ').split(' ')
+# print(user_list)
+for element in range(0, len(user_list) if len(user_list) % 2 == 0 else len(user_list) - 1, 2):
+    user_list[element], user_list[element + 1] = user_list[element + 1], user_list[element]
+print(user_list)
