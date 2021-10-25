339b4624a98d3462ca2ddff2a1a84f381a2bc7eb
Alex <stelmah1990@gmail.com>
2021-10-25 18:20:19 +0300

    added Task3

--- Task_3.py
+++ Task_3.py
@@ -1,6 +1,11 @@
-n = input('Введите число: ')
-abs_n = abs(int(n))
-num2 = int(str(abs_n) * 2)
-num3 = int(str(abs_n) * 3)
-res = abs_n + num2 + num3
-print(abs_n, '+', num2, '+', num3, '=', res)
+while True:
+    month = input('Введите месяц от 1 до 12: ')
+    if month.isdecimal() and 0 < int(month) < 13:
+        calendar = {'Зима': [12, 1, 2], 'Весна': [3, 4, 5], 'Лето': [6, 7, 8], 'Осень': [9, 10, 11]}
+        for key, val in calendar.items():
+            if int(month) in val:
+                print(key)
+                break
+        break
+    else:
+        print('Вы ввели некоорректные данные')
