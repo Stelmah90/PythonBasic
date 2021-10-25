34f94ae72cfc3c054078da541256b523aff44420
Alex <stelmah1990@gmail.com>
2021-10-25 19:44:40 +0300

    added Task6

--- Task_6.py
+++ Task_6.py
@@ -1,3 +1,23 @@
-my_list = [i for i in range(100, -5, -5)]
-my_list.append(int(input('Введите число для рейтинга - ')))
-print("Обновлённый список рейтига - {}".format(my_list.sort(reverse=True)))
+template = []
+cnt = 1
+flag = 'y'
+while flag.lower() == 'y':
+    template.append((cnt, {
+        'название': input('Введите название товара: '),
+        'цена': float(input('Введите цену товара: ')),
+        'колличество': int(input('Введите количество: ')),
+        'ед': input('единица измерения? ')
+    }))
+    cnt += 1
+    flag = input('Если хотите продолжить нажмите Y/ Закончить - любую клавишу')
+# print(template)
+res_dict = {
+    'название': [],
+    'цена': [],
+    'колличество': [],
+    'ед': []
+}
+for elem in range(0, len(template)):
+    for key, value in template[elem][1].items():
+        res_dict[key].append(value)
+print(res_dict)
