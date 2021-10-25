deb12ba8b7c1ffa9d3c2cb944034f713110f10d1
Alex <stelmah1990@gmail.com>
2021-10-25 18:42:19 +0300

    added Task4

--- Task_4.py
+++ Task_4.py
@@ -1,14 +1,7 @@
-while True:
-    n = int(input('Введите положительно число: '))
-    if n < 0:
-        print('Число отрицательное')
-    else:
-        res = 0
-        while n > 1:
-            div_n = n % 10
-            n //= 10
-            if div_n > res:
-                res = div_n
-        print('Самая большая цифра: ', res)
-        break
-
+user_string = input('Введите строку из нескольких слов через пробел: ').split(' ')
+# print(user_string)
+cnt = 1
+for word in user_string:
+    if word:
+        print("{}. {}".format(cnt, word if len(word) < 11 else word[0:10]))
+        cnt += 1
