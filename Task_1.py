86c90c5177158180e86bd91009f29848e8d76f32
Alex <stelmah1990@gmail.com>
2021-10-28 15:41:19 +0300

    test

--- Task_1.py
+++ Task_1.py
@@ -1,3 +1,2 @@
-task_list = [1, 123.12, 'test', (1, 2, 3), {1, 2, 3}, {'key_1': 'val_1', 'key_2': 'val_2'}, [1, 2, 3], bytes(12), None]
-for element in task_list:
-    print(type(element))
+print('test')
+
